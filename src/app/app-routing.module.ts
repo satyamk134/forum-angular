31719f0f0d0b0c143e709e749e9aa80788108811
textmercato <satyam@textmercato.com>
2020-02-25 17:58:35 +0530

    dash component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashComponent } from './dash/dash.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: DashComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
